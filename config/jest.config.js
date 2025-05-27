48172876e463d3362eb32b4a7b300e39b2f4853f
Joel Wickard <jwickard@gmail.com>
2025-05-26 22:07:56 -0500

    refactor jest config to run all tests with npm test

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,17 +3,34 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: '../',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@main/(.*)$': '<rootDir>/src/main/$1'
   },
   setupFiles: ['<rootDir>/config/jest.setup.js'],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/renderer/__tests__/jest.setup.renderer.js'
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.test.[jt]s?(x)',
+    '<rootDir>/src/**/__tests__/**/*.spec.[jt]s?(x)'
   ],
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   transform: {
     '^.+\.(ts|tsx)$': 'ts-jest'
   },
   transformIgnorePatterns: [
     'node_modules/(?!(better-sqlite3)/)'
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.vite/',
+    '/dist/'
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.vite/',
+    '/dist/',
+    '/__tests__/'
   ]
 };
